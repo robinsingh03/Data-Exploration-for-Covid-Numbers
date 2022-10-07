86a14568a84525a68f7db85d1d70d551acfa3c87
robinsingh03 <114664474+robinsingh03@users.noreply.github.com>
2022-10-08 00:43:16 +0530

    Update CovidQuery.sql

--- CovidQuery.sql
+++ CovidQuery.sql
@@ -1,29 +1,29 @@
-select location, date, total_cases, new_cases, total_deaths, population from [Portfolio Project]..CovidDeaths
+Select location, date, total_cases, new_cases, total_deaths, population from [Portfolio Project]..CovidDeaths
 order by location, date
 
 
 --Looking at Total Cases v/s Total Deaths
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage from [Portfolio Project]..CovidDeaths
+Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage from [Portfolio Project]..CovidDeaths
 where location = 'India'
 order by location, date
 
 
 --Looking at Total Cases v/s Population
 --Shows what percentage of Population got covid
-select location, date, total_cases, population, (total_cases/population)*100 as InfectedPopulationPercent from [Portfolio Project]..CovidDeaths
+Select location, date, total_cases, population, (total_cases/population)*100 as InfectedPopulationPercent from [Portfolio Project]..CovidDeaths
 where location = 'India'
 order by location, date
 
 
 --Looking at countries with most Infection rate compared to Population
-select location, population, max(total_cases) as HighestInfectCount, max((total_cases/population))*100 as InfectedPopulationPercent
+Select location, population, max(total_cases) as HighestInfectCount, max((total_cases/population))*100 as InfectedPopulationPercent
 from [Portfolio Project]..CovidDeaths
 where continent is not NULL
 group by location, population
 order by InfectedPopulationPercent desc
 
 --Looking at countries with most Deaths compared to population
-select location, population, max(cast(total_deaths as int)) as TotalDeathCount
+Select location, population, max(cast(total_deaths as int)) as TotalDeathCount
 from [Portfolio Project]..CovidDeaths
 where continent is not NULL
 group by location, population
@@ -32,7 +32,7 @@ order by TotalDeathCount desc
 
 --Breaking things down by Continent
 --Continents with highest Death Count
-select continent, max(cast(total_deaths as int)) as TotalDeathCount
+Select continent, max(cast(total_deaths as int)) as TotalDeathCount
 from [Portfolio Project]..CovidDeaths
 where continent is not null
 group by continent
@@ -40,7 +40,7 @@ order by TotalDeathCount desc
 
 
 --Global Numbers
-select date, sum(new_cases) as NewCases, sum(cast(new_deaths as int)) as NewDeaths, 
+Select date, sum(new_cases) as NewCases, sum(cast(new_deaths as int)) as NewDeaths, 
 (sum(cast(new_deaths as int))/sum(new_cases))*100 as DeathPercent 
 from [Portfolio Project]..CovidDeaths
 where continent is not null
@@ -52,18 +52,18 @@ order by 1, 2
 With Pop_vs_Vac(Continent, Location, Date, Population, New_Vaccinations, RollingCountVaccinations)
 as
 (
-select cv.continent, cv.location, cv.date, cd.population, cv.new_vaccinations,
+Select cv.continent, cv.location, cv.date, cd.population, cv.new_vaccinations,
 sum(cast(new_vaccinations as bigint)) over(partition by cv.location order by cv.location, cv.date) as RollingCountVaccinations
 from [Portfolio Project]..CovidVaccinations cv join [Portfolio Project]..CovidDeaths cd
 on cv.location = cd.location and cv.date = cd.date
 --where cv.continent is not null
 )
-select *, (RollingCountVaccinations/Population)*100 as PercentPopulationVaccinated from Pop_vs_Vac
+Select *, (RollingCountVaccinations/Population)*100 as PercentPopulationVaccinated from Pop_vs_Vac
 
 
 --Creating Views for storing useful Data
 Create view PercentPopulationVaccinated as
-select cv.continent, cv.location, cv.date, cd.population, cv.new_vaccinations,
+Select cv.continent, cv.location, cv.date, cd.population, cv.new_vaccinations,
 sum(cast(new_vaccinations as bigint)) over(partition by cv.location order by cv.location, cv.date) as RollingCountVaccinations
 from [Portfolio Project]..CovidVaccinations cv join [Portfolio Project]..CovidDeaths cd
 on cv.location = cd.location and cv.date = cd.date
